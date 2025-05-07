c07cb0bb3c852512a98994763f7afd46d6ee5dfc
David Moser <david.alexander@gmx.ch>
2025-05-07 13:35:07 +0200

    Update Readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,12 @@ dotenv.config();
 dotenv.config({ path: ".env.local" });
 
 const localhost_url = process.env.LCOALHOST || "";
-const localhost_user_1_private_key = process.env.LOCALHOST_USER_1_PRIVATE_KEY || "";
-const localhost_user_2_private_key = process.env.LOCALHOST_USER_2_PRIVATE_KEY || "";
-const localhost_user_3_private_key = process.env.LOCALHOST_USER_3_PRIVATE_KEY || "";
+const localhost_user_1_private_key =
+  process.env.LOCALHOST_USER_1_PRIVATE_KEY || "";
+const localhost_user_2_private_key =
+  process.env.LOCALHOST_USER_2_PRIVATE_KEY || "";
+const localhost_user_3_private_key =
+  process.env.LOCALHOST_USER_3_PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -28,7 +31,11 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       url: localhost_url,
-      accounts: [localhost_user_1_private_key, localhost_user_2_private_key, localhost_user_3_private_key],
+      accounts: [
+        localhost_user_1_private_key,
+        localhost_user_2_private_key,
+        localhost_user_3_private_key,
+      ],
     },
     // TODO - Add Live Testnet for VRF
   },
